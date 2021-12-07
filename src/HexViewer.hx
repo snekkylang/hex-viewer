3448a843ba7130d17583979ee99ead18e36486cd
oskar2517 <12718920+oskar2517@users.noreply.github.com>
2021-12-07 23:40:23 +0100

    add ability to pass file via command line

--- src/HexViewer.hx
+++ src/HexViewer.hx
@@ -9,7 +9,19 @@ class HexViewer {
 
     static function main() {
         #if sys
+        #if eval
         final path = "test.bite";
+        #else
+        final args = Sys.args();
+
+        if (args.length < 1) {
+            Sys.println("Usage: hexviewer <file>");
+            Sys.exit(1);
+        }
+
+        final path = args[0];
+        #end
+
         final bytes = sys.io.File.getBytes(path);
         printBytes(bytes);
         #end
